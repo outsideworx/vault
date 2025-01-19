0e92e501d780cb79e0916ebf4ce8d2dad4402b5d
hornokattila <hornokattila@outlook.com>
2025-01-19 02:11:23 +0100

    last path segment 7.

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ export default {
         const stmt = DATABASE.prepare('SELECT * FROM '.concat(pathSegment));
         const {results} = await stmt.all();
 
-        const module = await import(`./src/${pathSegment}`)
+        const module = await import(`./src/${pathSegment}.js`)
         return new Response(
             module.render(JSON.stringify(results, null, 2)),
             {
