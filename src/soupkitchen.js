e4fecc800be5b51654ab4fc56b972b99d16c7791
hornokattila <hornokattila@outlook.com>
2025-01-19 04:25:27 +0100

    added image FINAL 3.

--- src/soupkitchen.js
+++ src/soupkitchen.js
@@ -11,7 +11,7 @@ export function render(content) {
       <body>
         <header>
           <img src="https://outsideworx.net/assets/img/portfolio/zinis.png" width="400"/>
-          <h1>🎉 Successfully connected to SOUPKITCHEN admin site.</h1>
+          <h1>🎉 Successfully connected to Soup Kitchen admin site.</h1>
         </header>
         <main>
           <code><span style="color: #0E838F">&gt; </span>SELECT * FROM soupkitchen;<br/><br/>${content}</code>
