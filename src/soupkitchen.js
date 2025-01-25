06d5dbb31081c7b886969f7443ddc0b98bf41e94
hornokattila <hornokattila@outlook.com>
2025-01-26 00:14:40 +0100

    Rename.

--- src/soupkitchen.js
+++ src/soupkitchen.js
@@ -5,7 +5,7 @@ export function render(content) {
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>SoupKitchen admin</title>
+        <title>Soup Kitchen admin</title>
         <style>
             body {
                 font-family: Arial, sans-serif;
