dfc5d304ab537c43f50dfeb17379e74fa0499481
hornokattila <hornokattila@outlook.com>
2025-02-16 03:31:51 +0100

    Login method.

--- src/main/java/application/controller/LoginController.java
+++ src/main/java/application/controller/LoginController.java
@@ -1,15 +1,22 @@
 package application.controller;
 
+import application.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 final class LoginController {
+    private final UserService userService;
+
     @GetMapping("/admin")
     ModelAndView index() {
         return new ModelAndView("admin");
@@ -17,11 +24,16 @@ final class LoginController {
 
     @PostMapping("/admin")
     ModelAndView login(@RequestParam String username, @RequestParam String password) {
-        if (username.equals("asd@asd")) {
+        try {
+            if (!userService.loadUserByUsername(username).getPassword().equals(password)) {
+                throw new BadCredentialsException("Invalid username or password.");
+            }
+        } catch (UsernameNotFoundException | BadCredentialsException ignore) {
             ModelAndView login = new ModelAndView("admin");
             login.addObject("error", true);
+            log.error("Unsuccessful login try username[{}], password[{}].", username, password);
             return login;
-        }
+            }
         return new ModelAndView("home");
     }
 }
