af7b0cefa380cc0ff18e36774e666ef7f4a5d925
hornokattila <hornokattila@outlook.com>
2025-03-06 02:21:07 +0100

    Renamed controller.

--- src/main/java/application/controller/ViewControllers.java
+++ src/main/java/application/controller/ViewControllers.java
@@ -5,7 +5,7 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-class ViewController implements WebMvcConfigurer {
+class ViewControllers implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
