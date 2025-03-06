f668b49f362008d4f1acdb319010844fac816422
hornokattila <hornokattila@outlook.com>
2025-03-06 11:26:23 +0100

    CIAFO specific login.

--- src/main/java/application/controller/HomeController.java
+++ src/main/java/application/controller/HomeController.java
@@ -3,19 +3,27 @@ package application.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @RestController
 @RequiredArgsConstructor
 class HomeController {
+    private final Pattern addressDomainPattern = Pattern.compile("(?<=@)[^.]+(?=\\.)");
 
     @GetMapping("/home")
     ModelAndView home() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        ModelAndView modelAndView = new ModelAndView("home");
-        modelAndView.addObject("username", authentication.getName());
-        return modelAndView;
+        String email = authentication.getName();
+        Matcher matcher = addressDomainPattern.matcher(email);
+        if (!matcher.find()) {
+            throw new UsernameNotFoundException("Invalid email address.");
+        }
+        return new ModelAndView(matcher.group(0));
     }
 }
