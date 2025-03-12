b7669a6d6e8d6358a534dd826a36d45ea14838f3
hornokattila <hornokattila@outlook.com>
2025-03-12 18:30:37 +0100

    Final for today.

--- src/main/java/application/converter/client/CiafoConverter.java
+++ src/main/java/application/converter/client/CiafoConverter.java
@@ -2,10 +2,15 @@ package application.converter.client;
 
 import application.converter.ItemsConverter;
 import application.entity.client.CiafoItem;
+import net.coobird.thumbnailator.Thumbnails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 import org.thymeleaf.util.StringUtils;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
@@ -30,19 +35,19 @@ public class CiafoConverter extends ItemsConverter {
                             .orElse(null));
                     byte[] image1 = getImageBytes(files, iterator, "image1");
                     if (Objects.nonNull(image1)) {
-                        item.setImage1(image1);
+                        item.setImage1(reduceQuality(image1));
                     }
                     byte[] image2 = getImageBytes(files, iterator, "image2");
                     if (Objects.nonNull(image2)) {
-                        item.setImage2(image2);
+                        item.setImage2(reduceQuality(image2));
                     }
                     byte[] image3 = getImageBytes(files, iterator, "image3");
                     if (Objects.nonNull(image3)) {
-                        item.setImage3(image3);
+                        item.setImage3(reduceQuality(image3));
                     }
                     byte[] image4 = getImageBytes(files, iterator, "image4");
                     if (Objects.nonNull(image4)) {
-                        item.setImage4(image4);
+                        item.setImage4(reduceQuality(image4));
                     }
                     return item;
                 })
@@ -77,5 +82,22 @@ public class CiafoConverter extends ItemsConverter {
                 })
                 .filter(bytes -> bytes.length > 0)
                 .orElse(null);
+    }
+
+    private byte[] reduceQuality(byte[] image) {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        try {
+            BufferedImage originalImage = ImageIO.read(new ByteArrayInputStream(image));
+            int width = originalImage.getWidth();
+            int height = originalImage.getHeight();
+            Thumbnails.of(originalImage)
+                    .size((int) (width * 0.66), (int) (height * 0.66))
+                    .outputQuality(0.33)
+                    .outputFormat("jpeg")
+                    .toOutputStream(outputStream);
+        } catch (IOException e) {
+            throw new IllegalStateException("Image compression failed.", e);
+        }
+        return outputStream.toByteArray();
     }
 }
