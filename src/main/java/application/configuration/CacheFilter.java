3dd99ec238e6fb9bf61b9cb1c2221e08c8cb85eb
hornokattila <hornokattila@outlook.com>
2025-03-27 15:00:20 +0100

    Added cache control policy for API calls.

---
+++ src/main/java/application/configuration/CacheFilter.java
@@ -0,0 +1,21 @@
+package application.configuration;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class CacheFilter extends HttpFilter {
+    @Override
+    public void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
+        if (request.getRequestURI().startsWith("/api")) {
+            response.setHeader("Cache-Control", "public, max-age=86400");
+        }
+        chain.doFilter(request, response);
+    }
+}
