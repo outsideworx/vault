5eb7ab27e58c875154043cb20a8c5c8925f86868
hornokattila <hornokattila@outlook.com>
2025-03-28 14:52:37 +0100

    Database localization.

---
+++ postgres.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ "$1" == "--download" ]; then
+    if [ -z "$2" ]; then
+        echo "Error: IP address is required as the second parameter!"
+        exit 1
+    fi
+
+    SERVER_IP="$2"
+    rm -rf /tmp/data;
+    scp -r root@"$SERVER_IP":/root/data /tmp;
+fi
