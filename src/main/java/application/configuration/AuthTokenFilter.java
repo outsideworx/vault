443f8ac7d3d79063897f214363f9d04123e530d9
hornokattila <hornokattila@outlook.com>
2025-09-26 13:27:46 +0200

    API exception tracking.

--- src/main/java/application/configuration/AuthTokenFilter.java
+++ src/main/java/application/configuration/AuthTokenFilter.java
@@ -1,6 +1,7 @@
 package application.configuration;
 
 import application.configuration.utils.FilterConditions;
+import application.service.GrafanaService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpFilter;
@@ -19,11 +20,14 @@ import java.io.IOException;
 final class AuthTokenFilter extends HttpFilter {
     private final FilterConditions filterConditions;
 
+    private final GrafanaService grafanaService;
+
     @Override
     public void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
         if (filterConditions.notPreflightRequest(request) && filterConditions.apiRequest(request)) {
             if (filterConditions.invalidCallerIdOrAuthToken(request)) {
                 log.error("Invalid caller id or auth token for request: [{}]", request.getRequestURL());
+                grafanaService.registerRequest("exception", "bad_credentials");
                 throw new BadCredentialsException("Invalid caller id or auth token.");
             }
         }
